dfae6a89dc02b7f816b655a0aedcc40e898504aa
fmatys <fabiomatys@gmail.com>
2024-10-10 15:18:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config(); // Carregar variáveis do .env
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Substitua pelo seu token do bot do Telegram
+const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+
+// Rota para enviar mensagem via Telegram (usando GET)
+app.get('/', async (req, res) => {
+    const {
+        chatId,
+        message
+    } = req.query;
+
+    if (!chatId || !message) {
+        console.log(`Requisição Recebida sem os parametros necessários`);
+        return res.status(400).json({
+            error: 'chatId e message são obrigatórios'
+        });
+    }
+
+    try {
+        const response = await axios.get(`${TELEGRAM_API_URL}/sendMessage`, {
+            params: {
+                chat_id: chatId,
+                text: message
+            }
+        });
+        console.log(`Menssagem envida: ${message} | Para: ${chatId}`);
+        return res.status(200).json({
+            success: true,
+            result: response.data
+        });
+    } catch (error) {
+        console.log(`ERRO ao enviar msg: ${message} | Para: ${chatId}`);
+        return res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`API rodando na porta ${PORT}`);
+});
