bb1aa7395e70fe3c437d582995b16a6080a92f6b
fmatys <fabiomatys@gmail.com>
2024-10-11 15:03:23 -0300

    teste contador

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
 const MY_CHAT = process.env.MY_CHATID;
 
+let cont = 0; // Inicializa o contador
+
 // Rota para enviar mensagem via Telegram (usando GET)
 app.get('/', async (req, res) => {
+    cont++;
     const {
         message
     } = req.query;
@@ -19,7 +22,7 @@ app.get('/', async (req, res) => {
     if (!message) {
         console.log(`Requisição Recebida sem os parametros necessários`);
         return res.status(400).json({
-            error: 'Parametro message é obrigatório'
+            error: 'Parametro message é obrigatório',
         });
     }
 
@@ -27,13 +30,14 @@ app.get('/', async (req, res) => {
         const response = await axios.get(`${TELEGRAM_API_URL}/sendMessage`, {
             params: {
                 chat_id: MY_CHAT,
-                text: message
+                text: message + " - " + cont
             }
         });
         console.log(`Menssagem envida: ${message} | Para: ${MY_CHAT}`);
         return res.status(200).json({
             success: true,
-            result: response.data
+            result: response.data,
+            contador: cont
         });
     } catch (error) {
         console.log(`ERRO ao enviar msg: ${message} | Para: ${MY_CHAT}`);
