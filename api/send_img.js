2cc8f65545af77ce6870b7252baa4fe873d541e6
fmatys <fabiomatys@gmail.com>
2024-10-11 17:57:30 -0300

    envio img & token

---
+++ api/send_img.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config(); // Carregar variáveis do .env
+
+const app = express();
+
+// Substitua pelo seu token do bot do Telegram
+const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+const MY_CHAT = process.env.MY_CHATID;
+const VALID_TOKEN = process.env.MY_VALID_TOKEN;
+
+
+// Middleware para verificar o token
+function checkToken(req, res, next) {
+    const token = req.query.token;
+
+    if (token !== VALID_TOKEN) {
+        return res.status(403).json({
+            success: false,
+            message: 'Acesso negado. Token inválido.'
+        });
+    }
+    next(); // Continua se o token for válido
+}
+
+
+// Rota para enviar imagem via Telegram (usando GET)
+app.get('/img', checkToken, async (req, res) => {
+    const {
+        image_url, // URL da imagem
+        caption // Legenda da imagem
+    } = req.query;
+
+    // Verificar se os parâmetros necessários foram fornecidos
+    if (!image_url || !caption) {
+        console.log('Requisição recebida sem os parâmetros necessários');
+        return res.status(400).json({
+            error: 'Os parâmetros image_url e caption são obrigatórios',
+        });
+    }
+
+    try {
+        // Enviar a imagem via Telegram
+        const response = await axios.get(`${TELEGRAM_API_URL}/sendPhoto`, {
+            params: {
+                chat_id: MY_CHAT,
+                photo: image_url, // URL da imagem a ser enviada
+                caption: caption // Legenda da imagem com contador
+            }
+        });
+
+        console.log(`Imagem enviada: ${image_url} | Legenda: ${caption} | Para: ${MY_CHAT}`);
+        return res.status(200).json({
+            success: true,
+            result: response.data,
+        });
+    } catch (error) {
+        console.log(`Erro ao enviar imagem: ${image_url} | Para: ${MY_CHAT}`);
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
+// Exportar o aplicativo para que a Vercel o use como uma função serverless
+module.exports = app;
--- api/send_txt.js
+++ api/send_txt.js
@@ -3,18 +3,28 @@ const axios = require('axios');
 require('dotenv').config(); // Carregar variáveis do .env
 
 const app = express();
-const PORT = process.env.PORT || 3300;
 
 // Substitua pelo seu token do bot do Telegram
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
 const MY_CHAT = process.env.MY_CHATID;
+const VALID_TOKEN = process.env.MY_VALID_TOKEN;
 
-let cont = 0; // Inicializa o contador
+// Middleware para verificar o token
+function checkToken(req, res, next) {
+    const token = req.query.token;
+
+    if (token !== VALID_TOKEN) {
+        return res.status(403).json({
+            success: false,
+            message: 'Acesso negado. Token inválido.'
+        });
+    }
+    next(); // Continua se o token for válido
+}
 
 // Rota para enviar mensagem via Telegram (usando GET)
-app.get('/', async (req, res) => {
-    cont++;
+app.get('/txt', checkToken, async (req, res) => {
     const {
         message
     } = req.query;
@@ -22,8 +32,7 @@ app.get('/', async (req, res) => {
     if (!message) {
         console.log(`Requisição Recebida sem os parametros necessários`);
         return res.status(400).json({
-            error: 'Parametro message é obrigatório',
-            contador: cont
+            error: 'O Parametro message é obrigatório',
         });
     }
 
@@ -31,25 +40,22 @@ app.get('/', async (req, res) => {
         const response = await axios.get(`${TELEGRAM_API_URL}/sendMessage`, {
             params: {
                 chat_id: MY_CHAT,
-                text: message + " - " + cont
+                text: message
             }
         });
         console.log(`Menssagem envida: ${message} | Para: ${MY_CHAT}`);
         return res.status(200).json({
             success: true,
             result: response.data,
-            contador: cont
         });
     } catch (error) {
         console.log(`ERRO ao enviar msg: ${message} | Para: ${MY_CHAT}`);
         return res.status(500).json({
             success: false,
             error: error.message,
-            contador: cont
         });
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`API rodando na porta ${PORT}`);
-});
+// Exportar o aplicativo para que a Vercel o use como uma função serverless
+module.exports = app;
